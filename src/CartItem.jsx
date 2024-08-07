0ebc49b4caf5ef632b51c3f52fa8124bf3cdda41
Saloni Sharma <mailsalonisharma@gmail.com>
2024-08-07 19:49:59 -0400

    updated cartitem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    //console.log(cart);
+    cart.map(item=>{
+        let itemCost = parseInt(item.cost.substring(1,item.cost.length));
+        totalAmount = totalAmount + (itemCost*item.quantity);
+    })
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
-
-  const handleIncrement = (item) => {
+const handleIncrement = (item) => {
+    dispatch(updateQuantity({...item, quantity: item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({...item, quantity: item.quantity-1}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let itemCost = parseInt(item.cost.substring(1,item.cost.length));
+    return (itemCost * item.quantity);
   };
 
   return (
